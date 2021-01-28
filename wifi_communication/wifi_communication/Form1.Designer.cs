6d5102d8759247111f698d1ac7d5e2f219b56356
Burak Güzel <44478688+BurakYtu@users.noreply.github.com>
2021-01-28 15:06:41 +0300

    UDP Read
    udp read var ama udp recive yok

---
+++ wifi_communication/wifi_communication/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace wifi_communication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.read_btn = new System.Windows.Forms.Button();
+            this.send_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(41, 113);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(254, 156);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ip";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "port";
+            // 
+            // read_btn
+            // 
+            this.read_btn.Location = new System.Drawing.Point(237, 28);
+            this.read_btn.Name = "read_btn";
+            this.read_btn.Size = new System.Drawing.Size(75, 23);
+            this.read_btn.TabIndex = 3;
+            this.read_btn.Text = "read";
+            this.read_btn.UseVisualStyleBackColor = true;
+            this.read_btn.Click += new System.EventHandler(this.read_btn_Click);
+            // 
+            // send_btn
+            // 
+            this.send_btn.Location = new System.Drawing.Point(237, 65);
+            this.send_btn.Name = "send_btn";
+            this.send_btn.Size = new System.Drawing.Size(75, 23);
+            this.send_btn.TabIndex = 3;
+            this.send_btn.Text = "send";
+            this.send_btn.UseVisualStyleBackColor = true;
+            this.send_btn.Click += new System.EventHandler(this.send_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 326);
+            this.Controls.Add(this.send_btn);
+            this.Controls.Add(this.read_btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button read_btn;
+        private System.Windows.Forms.Button send_btn;
+    }
+}
+
---
+++ wifi_communication/wifi_communication/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace wifi_communication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            UdpClient Client = new UdpClient(8800); //this device Port number (uygulama ayarlar sekmesinde remoto port yerine bunu yazılacak)
+                                                                            //(uygulamada ayarlar sekmesindeki remote addr yerine, pc de cmd ye girip ipconfig yazılmalı ve çıkan ipv4 adresi girilmeli)
+            InitializeComponent();
+        }
+
+        private void send_btn_Click(object sender, EventArgs e)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPAddress broadcast = IPAddress.Parse("192.168.1.45"); //slave adress (Uygulamada Sol yukarda yazan yani telefonun ip si)
+
+            byte[] sendbuf = Encoding.ASCII.GetBytes(richTextBox1.Text);
+
+            IPEndPoint ep = new IPEndPoint(broadcast, 8080); //slave adress, slave port(uygulama ayarlar sekmesindeki local port)
+
+            s.SendTo(sendbuf, ep);
+        }
+
+        private void read_btn_Click(object sender, EventArgs e)
+        {
+            UdpClient recieveClient = new UdpClient(8800);
+
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 8080);
+            try
+            {
+                // Blocks until a message returns on this socket from a remote host.
+                Byte[] receiveBytes = recieveClient.Receive(ref RemoteIpEndPoint);
+
+                string Data = Encoding.ASCII.GetString(receiveBytes);
+
+                richTextBox1.Text = Data.ToString();
+            }
+            catch (Exception)
+            {
+                Close();
+            }
+        }
+    }
+}
